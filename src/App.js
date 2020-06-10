aa217522267a3b11c6f5c241f6e7979fcdca45ae
jli3238 <jli3238@gmail.com>
2020-06-10 01:09:44 -0500

    Add Kanban_dnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Column from '../src/Column';
+import Card from '../src/Card';
+import './index.css';
 
-function App() {
+const App = () => {
+  const [columns, setColumns] = useState([]);
+  const [newCardName, setNewCardName] = useState('');
+  const columnsApi = 'data/columns.json';
+  useEffect(() => {
+      try {
+          fetch(columnsApi)
+              .then(response => response.json())
+              .then(
+                  result => setColumns(result),
+                  error => console.log(error));
+      } catch (error) { console.log(error); }
+  }, []);
+  const handleNewCardNameChange = (e) => {
+    const { target } = e;
+    if (target.value.trim() ==='') return;
+    setNewCardName(target.value.trim());
+  }
+  const handleAddNewCard = (columnId) => {
+    if (newCardName.trim() ==='') return;
+    const cardId = columns.find(column =>column.id === columnId).cards.length;
+    const newCard = {id: cardId, name: newCardName };
+    const updatedCards = [...columns.find(column => column.id === columnId).cards, newCard];
+    const updatedColumn = {...columns.find(column => column.id === columnId), cards: updatedCards};
+    const updatedColumns = columns.map(column => column.id === columnId ? updatedColumn : column);
+    setColumns(updatedColumns);
+    setNewCardName('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className='board'>
+        { columns.map(column =>
+          <div className='column'>
+            <h3>{column.name}</h3>
+            <input type='text' value={newCardName} onChange={e => handleNewCardNameChange(e)} placeholder='Enter new card name'/>
+            <button onClick={() => handleAddNewCard(column.id)}>+</button>
+            <Column 
+              id={column.id}
+              key={column.id}
+              >
+              {column.cards.map(card =>
+                <Card 
+                  id={card.id}
+                  key={card.id}
+                  >
+                  {card.name}
+                </Card>
+              )}
+            </Column>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
