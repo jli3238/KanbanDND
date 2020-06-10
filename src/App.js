203565bc099f17b1f827fc2e88afb649c93fa3b8
jli3238 <jli3238@gmail.com>
2020-06-10 10:24:35 -0500

    Clean up

--- src/App.js
+++ src/App.js
@@ -8,23 +8,20 @@ const App = () => {
   const [newCardName, setNewCardName] = useState('');
   const columnsApi = 'data/columns.json';
   useEffect(() => {
-      try {
-          fetch(columnsApi)
-              .then(response => response.json())
-              .then(
-                  result => setColumns(result),
-                  error => console.log(error));
+      try { fetch(columnsApi)
+        .then(response => response.json())
+        .then(result => setColumns(result), error => console.log(error));
       } catch (error) { console.log(error); }
   }, []);
   const handleNewCardNameChange = (e) => {
-    const { target } = e;
-    if (target.value.trim() ==='') return;
-    setNewCardName(target.value.trim());
+    if (e.target.value.trim() ==='') return;
+    setNewCardName(e.target.value.trim());
   }
   const handleAddNewCard = (columnId) => {
-    if (newCardName.trim() ==='') return;
-    const cardId = columns.find(column =>column.id === columnId).cards.length;
-    const newCard = {id: cardId, name: newCardName };
+    if (newCardName ==='') return;
+    let newCardId = 0;
+    for (let i=0; i<columns.length; i++) { newCardId += columns[i].cards.length; }
+    const newCard = {id: newCardId, name: newCardName };
     const updatedCards = [...columns.find(column => column.id === columnId).cards, newCard];
     const updatedColumn = {...columns.find(column => column.id === columnId), cards: updatedCards};
     const updatedColumns = columns.map(column => column.id === columnId ? updatedColumn : column);
@@ -35,22 +32,12 @@ const App = () => {
     <div className="App">
       <main className='board'>
         { columns.map(column =>
-          <div className='column'>
+          <div key={column.id} className='column'>
             <h3>{column.name}</h3>
             <input type='text' value={newCardName} onChange={e => handleNewCardNameChange(e)} placeholder='Enter new card name'/>
             <button onClick={() => handleAddNewCard(column.id)}>+</button>
-            <Column 
-              id={column.id}
-              key={column.id}
-              >
-              {column.cards.map(card =>
-                <Card 
-                  id={card.id}
-                  key={card.id}
-                  >
-                  {card.name}
-                </Card>
-              )}
+            <Column id={column.id}>
+              {column.cards.map(card =><Card key={card.id} id={card.id}>{card.name}</Card>)}
             </Column>
           </div>
         )}
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,24 +3,18 @@ import React from 'react';
 const Card = (props) => {
     const dragStart = e => {
         const { target } = e;
-        e.dataTransfer.setData('card_id', target.id);
-        setTimeout(() => {
-            target.style.display = 'none';
-        }, 0)
+        e.dataTransfer.setData('cardId', target.id);
+        setTimeout(() => { target.style.display = 'none';  }, 0)
     }
-    const dragOver = e => {
-        e.stopPropagation();
-    }
-    return (
-        <div 
-            id={props.id}
-            className='card'
-            draggable={true}
-            onDragStart={dragStart}
-            onDragOver={dragOver}>
-            { props.children }
-        </div>
-    )
+    const dragOver = e => { e.stopPropagation(); }
+    return (<div 
+        id={props.id}
+        className='card'
+        draggable={true}
+        onDragStart={dragStart}
+        onDragOver={dragOver}>
+        { props.children }
+    </div>)
 }
 
 export default Card;
